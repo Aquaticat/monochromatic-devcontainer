b765a15decbd5e926ebcab01d12c28a3c42cb0d9
Aquaticat <an@aquati.cat>
2024-04-27 23:27:18 -0400

    fix: zx xcaddy build

--- src/index.js
+++ src/index.js
@@ -188,15 +188,15 @@ hyperlink: auto
         await $`go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest`;
 
         await $`xcaddy build ${[
-          '--with github.com/mholt/caddy-events-exec',
-          '--with github.com/mholt/caddy-webdav',
-          '--with github.com/mholt/caddy-l4',
-          '--with github.com/porech/caddy-maxmind-geolocation',
-          '--with github.com/mholt/caddy-ratelimit',
-          '--with github.com/caddyserver/cache-handler',
-          '--with github.com/caddyserver/jsonc-adapter',
-          '--with github.com/caddy-dns/porkbun',
-          '--with github.com/caddy-dns/njalla',
+          '--with', 'github.com/mholt/caddy-events-exec',
+          '--with', 'github.com/mholt/caddy-webdav',
+          '--with', 'github.com/mholt/caddy-l4',
+          '--with', 'github.com/porech/caddy-maxmind-geolocation',
+          '--with', 'github.com/mholt/caddy-ratelimit',
+          '--with', 'github.com/caddyserver/cache-handler',
+          '--with', 'github.com/caddyserver/jsonc-adapter',
+          '--with', 'github.com/caddy-dns/porkbun',
+          '--with', 'github.com/caddy-dns/njalla',
         ]}`
           .pipe(process.stdout);
 
